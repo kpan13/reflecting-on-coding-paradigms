19528b44701cfc522336c596ed5c6c44441aba7c
kpan13 <kajaljpan@gmail.com>
2024-09-10 11:40:10 -0700

    add two files

---
+++ main.py
@@ -0,0 +1,7 @@
+#Getting Started
+#Create a Replit named Reflecting on Coding Paradigms using the Python template.
+#Do your work and write your answers for the following prompts in the generated main.py file.
+#For the thought prompts, write your answers as comments.
+#As you work through the prompts, be sure to add comments titling what section each solution is for.
+
+
